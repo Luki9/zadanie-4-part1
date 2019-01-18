d107c06c03d7bdf502c017ebee694474a028a268
lukasz <robson27-r@hotmail.com>
2019-01-18 18:25:16 +0100

    zadanie zrobione

---
+++ src/Televisor.java
@@ -0,0 +1,23 @@
+ class Televisor {
+
+
+    private boolean status;
+
+    void turnOff(){
+        status = false;
+    }
+
+    void turnOn(){
+        status = true;
+
+    }
+    void showStatus(){
+        System.out.println("Status" + status);
+
+
+
+     }
+
+
+}
+
---
+++ src/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+
+        Televisor televisor1 = new Televisor();
+
+        televisor1.showStatus();
+
+        televisor1.turnOff();
+        televisor1.showStatus();
+
+        televisor1.turnOn();
+        televisor1.showStatus();
+
+
+    }
+}
